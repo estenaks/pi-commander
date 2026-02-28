7f2e84e8892f62551379008a330e79f0c8553706
estenaks <aksel.estensen@tietoevry.com>
2026-02-28 12:43:18 +0100

    fix gpu issues

--- utils/open_url.sh
+++ utils/open_url.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-URL="http://127.0.0.1/face"
+URL="http://127.0.0.1/face?rotate=1"
+# ^ if display is landscape this shows image in portrait
 
 # If you're on the desktop (X11), these help Chromium start from cron/autostart
 export DISPLAY=:0
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 
-# Launch Chromium full-screen with reduced UI noise
-/usr/bin/chromium-browser \
+exec /usr/bin/chromium-browser \
   --new-window \
   --disable-session-crashed-bubble \
   --disable-infobars \
   --start-fullscreen \
+  --disable-gpu \
+  --disable-software-rasterizer \
+  --use-gl=swiftshader \
   "$URL"
