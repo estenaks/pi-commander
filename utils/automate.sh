930738deb99c8953433883675a6d0edf9940a6b3
estenaks <aksel.estensen@tietoevry.com>
2026-02-28 13:49:19 +0100

    added qr func

--- utils/automate.sh
+++ utils/automate.sh
@@ -4,3 +4,6 @@ chmod +x $dir/add_nginx.sh
 chmod +x $dir/open_url.sh
 mkdir -p /home/pi/.config/lxsession/LXDE-pi
 cp $dir/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo mkdir -p /etc/lightdm/lightdm.conf.d
+cp $dir/50-nocursor.conf /etc/lightdm/lightdm.conf.d/50-nocursor.conf
+sudo systemctl restart lightdm
