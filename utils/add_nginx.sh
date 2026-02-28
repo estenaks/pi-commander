8eb117e4a7d7a8c783c7b569c063f347a2020146
estenaks <aksel.estensen@tietoevry.com>
2026-02-28 12:28:53 +0100

    added and automated nginx setup

---
+++ utils/add_nginx.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo systemctl enable nginx
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-available/default
+sudo nano /etc/nginx/sites-available/pi-commander
+sudo cat /home/pi/pi-commander/utils/nginx-site > /etc/nginx/sites-available/pi-commander
+sudo ln -sf /etc/nginx/sites-available/pi-commander /etc/nginx/sites-enabled/pi-commander
+sudo nginx -t
+sudo systemctl reload nginx
