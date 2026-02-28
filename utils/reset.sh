c95874aeea6d3a6e4d03e7ec4378ab992adba051
estenaks <aksel.estensen@tietoevry.com>
2026-02-28 14:03:03 +0100

    update

--- utils/reset.sh
+++ utils/reset.sh
@@ -1,6 +1,6 @@
 dir="/home/pi/pi-commander/utils"
 cd $dir
 git pull
-$dir/kill_chromium.sh
-sudo systemctl restart pi-commander-webserver
-$dir/open_url.sh
+$dir/kill_chromium.sh >/dev/null 2>&1
+sudo systemctl restart pi-commander-webserver >/dev/null 2>&1
+$dir/open_url.sh >/dev/null 2>&1
