92fbf2034417e9de09fd87b7cab1ee19f3db395e
estenaks <aksel.estensen@tietoevry.com>
2026-02-28 12:20:38 +0100

    update service script

--- utils/add_service.sh
+++ utils/add_service.sh
@@ -1,1 +1,4 @@
-sudo cat /home/pi/pi-commander/utils/pi-commander-webserver.service > /etc/systemd/system/pi-commander-webserver.service 
+sudo cat /home/pi/pi-commander/utils/pi-commander-webserver.service > /etc/systemd/system/pi-commander-webserver.service
+sudo systemctl daemon-reload
+sudo systemctl enable pi-commander-webserver.service
+sudo systemctl start pi-commander-webserver.service 
