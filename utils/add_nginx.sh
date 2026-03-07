d1d739a0b2705d22409e87686beeef6fad36dc66
estenaks <aksel.estensen@tietoevry.com>
2026-03-07 08:12:58 +0100

    fix

--- utils/add_nginx.sh
+++ utils/add_nginx.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y nginx
 sudo systemctl enable nginx
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo rm -f /etc/nginx/sites-available/default
-sudo nano /etc/nginx/sites-available/pi-commander
+sudo touch /etc/nginx/sites-available/pi-commander
 sudo cp /home/pi/pi-commander/utils/nginx-site > /etc/nginx/sites-available/pi-commander
 sudo ln -sf /etc/nginx/sites-available/pi-commander /etc/nginx/sites-enabled/pi-commander
 sudo nginx -t
--- utils/automate.sh
+++ utils/automate.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 dir="/home/pi/pi-commander/utils"
 chmod +x $dir/add_service.sh
 chmod +x $dir/add_nginx.sh
@@ -7,7 +8,10 @@ cp $dir/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
 sudo mkdir -p /etc/lightdm/lightdm.conf.d
 sudo cp $dir/50-nocursor.conf /etc/lightdm/lightdm.conf.d/50-nocursor.conf
 sudo systemctl restart lightdm
-#!/bin/sh
+
+$dir/add_nginx.sh
+$dir/add_service.sh
+
 read -r -p "Changes will take effect after rebooting. Reboot NOW? [y/N] " ans
 case "$ans" in
   y|Y|yes|YES)
