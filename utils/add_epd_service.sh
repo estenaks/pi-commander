7b2d159d28baa2509bb07562e1e0919bd5bab461
estenaks <aksel.estensen@tietoevry.com>
2026-03-07 09:44:59 +0100

    fix

--- utils/add_epd_service.sh
+++ utils/add_epd_service.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SERVICE=epd-commander
 UNIT=/etc/systemd/system/${SERVICE}.service
 REPO=/home/pi/pi-commander
-
+sudo touch "${UNIT}"
 sudo cp "${REPO}/utils/${SERVICE}.service" "${UNIT}"
 sudo systemctl daemon-reload
 sudo systemctl enable "${SERVICE}.service"
