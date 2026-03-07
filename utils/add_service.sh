41e1e8eea062d0f6efe654cddbf5008c3fab2996
estenaks <aksel.estensen@tietoevry.com>
2026-03-07 08:16:06 +0100

    fixxxx

--- utils/add_service.sh
+++ utils/add_service.sh
@@ -1,4 +1,5 @@
-sudo cat /home/pi/pi-commander/utils/pi-commander-webserver.service > /etc/systemd/system/pi-commander-webserver.service
+sudo touch /etc/systemd/system/pi-commander-webserver.service
+sudo cp /home/pi/pi-commander/utils/pi-commander-webserver.service > /etc/systemd/system/pi-commander-webserver.service
 sudo systemctl daemon-reload
 sudo systemctl enable pi-commander-webserver.service
 sudo systemctl start pi-commander-webserver.service 
