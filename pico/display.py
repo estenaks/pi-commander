0bcfc3c145d88b9ea3342e0e7fb15a79b781a53d
estenaks <aksel.estensen@tietoevry.com>
2026-03-08 16:00:50 +0100

    asdf

--- pico/display.py
+++ pico/display.py
@@ -1,6 +1,6 @@
 import urequests, gc
 
-def show_image(lcd, server, player=1, face="front"):
+def show_image(lcd, server, player=2, face="front"):
     """Fetch and display a card image from the server, one strip at a time."""
     for strip_idx, show_fn in ((0, lcd.show_up), (1, lcd.show_mid), (2, lcd.show_down)):
         url = "{}/img/{}/{}/raw?strip={}".format(server, player, face, strip_idx)
